562f70ffba30ba2bf53ce3d5c2d276ae5d741a29
nagaraju <tnr.mtech@gmail.com>
2020-05-23 09:43:44 +0530

    Updates

---
+++ precache-manifest.7a020d046f548ba3d2bb33d7fcfb7d58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a033614d7030b16fcb63",
+    "url": "/webpage/static/css/main.0d800f07.chunk.css"
+  },
+  {
+    "revision": "a033614d7030b16fcb63",
+    "url": "/webpage/static/js/main.a033614d.chunk.js"
+  },
+  {
+    "revision": "a3979c57b863725ec047",
+    "url": "/webpage/static/css/1.4a51ce96.chunk.css"
+  },
+  {
+    "revision": "a3979c57b863725ec047",
+    "url": "/webpage/static/js/1.a3979c57.chunk.js"
+  },
+  {
+    "revision": "35c6d35963d4e3bd941d",
+    "url": "/webpage/static/js/runtime~main.35c6d359.js"
+  },
+  {
+    "revision": "d17a2c852e61467137eccedaefef257f",
+    "url": "/webpage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2717c5b056098fcce825f7836306a09a.js"
+  "/webpage/precache-manifest.7a020d046f548ba3d2bb33d7fcfb7d58.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/webpage/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
