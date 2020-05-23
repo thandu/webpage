3328f3dd94ed406e0709284d44c45415b802ad03
nagaraju <tnr.mtech@gmail.com>
2020-05-23 09:20:45 +0530

    Updates

---
+++ precache-manifest.2717c5b056098fcce825f7836306a09a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6828ac0469daab580db",
+    "url": "/static/css/main.0d800f07.chunk.css"
+  },
+  {
+    "revision": "d6828ac0469daab580db",
+    "url": "/static/js/main.d6828ac0.chunk.js"
+  },
+  {
+    "revision": "a3979c57b863725ec047",
+    "url": "/static/css/1.4a51ce96.chunk.css"
+  },
+  {
+    "revision": "a3979c57b863725ec047",
+    "url": "/static/js/1.a3979c57.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b29d533eec4198324cdab3f58f3d1d0",
+    "url": "/index.html"
+  }
+];
